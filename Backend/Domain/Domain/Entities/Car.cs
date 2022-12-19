b7871486852289a687119ac47cf4fcd80fec8023
RosaFlavius <flavius.rosa@amdaris.com>
2022-12-19 20:41:11 +0200

    Created entities, enums and repo patterns

---
+++ Backend/Domain/Domain/Entities/Car.cs
@@ -0,0 +1,41 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Car
+    {
+        public Car(string make, string model, int year, int mileage, FuelType fuel, float price, string image)
+        {
+            Fuel = fuel;
+
+            if (string.IsNullOrEmpty(make))
+                throw new ArgumentNullException("Null_make");
+            if (string.IsNullOrEmpty(model))
+                throw new ArgumentNullException("Null_model");
+            if (year < 1800)
+                throw new ArgumentOutOfRangeException("Wrong_Year_Range");
+            if (mileage < 0)
+                throw new ArgumentOutOfRangeException("Wrong_Mileage_Range");
+            if (string.IsNullOrEmpty(image))
+                throw new ArgumentNullException("Null_image");
+            if (price <= 0)
+                throw new ArgumentOutOfRangeException("Wrong_Price_Range");
+        }
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int Mileage { get; set; }
+        public FuelType Fuel { get; set; }
+        public string Image { get; set; }
+        public float Price { get; set; }
+
+
+
+    }
+}
---
+++ Backend/Domain/Domain/RepositoryPatterns/IOrderRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.RepositoryPatterns
+{
+    public interface IOrderRepository
+    {
+        public void AddOrder(Order order);
+        public bool DeleteOrder(Guid orderId);
+        public Order UpdateOrder(Order Order);
+        public Task<Order> GetOrder(Guid orderId);
+        public Task<IEnumerable<Order>> GetAllOrders();
+        public Task<int> SaveChangesAsync();
+    }
+}
