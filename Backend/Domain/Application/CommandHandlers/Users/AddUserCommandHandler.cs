68af439216d32a97446af31a18df5e5eb8acf4f3
RosaFlavius <flavius.rosa@amdaris.com>
2022-12-19 22:33:36 +0200

    Added dbContext to infrastructure plus repo/commands/commandHandlers for user

---
+++ Backend/Domain/Application/CommandHandlers/Users/AddUserCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using Application.Commands;
+using Application.Repositories;
+using Domain.Entities;
+using Domain.RepositoryPattern;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.CommandHandlers
+{
+    public class AddUserCommandHandler : IRequestHandler<AddUserCommand, User>
+    {
+        private readonly IUserRepository _userRepo;
+        public AddUserCommandHandler(IUserRepository userRepo) 
+        {
+            _userRepo = userRepo;
+        }
+        public async Task<User> Handle(AddUserCommand request, CancellationToken cancellationToken)
+        {
+
+            var user = new User(
+                request.FirstName,
+                request.LastName,
+                request.Email,
+                request.Password,
+                request.DateOfBirth,
+                request.Phone,
+                request.Country,
+                request.City,
+                request.Address,
+                request.Admin
+                );
+
+            _userRepo.AddUser(user);
+            await _userRepo.SaveChangesAsync();
+            return user;
+        }
+    }
+}
---
+++ Backend/Domain/Infrastructure/Data/DataDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class DataDbContext : DbContext
+    {
+        public DataDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Car> Car { get; set; }
+    }
+}
